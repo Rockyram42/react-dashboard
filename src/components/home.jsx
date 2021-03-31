9cebcffee5fd6cd9eae8d5defe57c3ccbee8af6f
ramanuja <ramanuja.dash42@gmail.com>
2021-03-31 18:31:35 +0530

    delete function issue fixed

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -15,6 +15,8 @@ class Home extends Component {
     }
      onClickHandelDelete = () => {
       console.log(value)
+      console.log("hi ");
+      
     }
     // useEffect(() => {
     //     props.fetchApiProductData();
